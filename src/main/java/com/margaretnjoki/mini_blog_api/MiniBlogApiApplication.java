b15ff8a6ec9a0d90b296fc7187f3c7c55892a45f
margaretnjoki <maggykiroko@gmail.com>
2026-08-07 12:21:35 +0300

    initial

---
+++ src/main/java/com/margaretnjoki/mini_blog_api/MiniBlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.margaretnjoki.mini_blog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniBlogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/margaretnjoki/mini_blog_api/MiniBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.margaretnjoki.mini_blog_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
